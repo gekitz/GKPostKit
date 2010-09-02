68e15b5ed0b1203193b7b42e0890f0c573c8cd53
Georg Kitz <georgkitz@gmail.com>
2010-09-02 18:02:28 +0200

    universal config file

--- Classes/GKPostKitGlobals.h
+++ Classes/GKPostKitGlobals.h
@@ -11,13 +11,13 @@
 
 //Facebook
 
-#define kFBApiKey @"fbaef1bd561b691cfabe1d8ca0ee530c"
-#define kFBApiSecret @"19da045ed0d2697b9f37c7c31d3849ff"
-#define kFBCallbackURL @"http://www.vooch.at/oauth_redirect/"
+#define kFBApiKey @""
+#define kFBApiSecret @""
+#define kFBCallbackURL @""
 
 //Twitter
 
-#define kTwitterApiKey @"G42oLc98IOGjJRyD8Opeg"
-#define kTwitterCallbackURL @"http://m.vooch.at"
-#define kTwitterConsumerKey @"G42oLc98IOGjJRyD8Opeg"
-#define kTwitterConsumerSecret @"xI6jS1AiwvrrLQUIwnXHDbMkfTE5RGlDAWr2Y7ip7w"
+#define kTwitterApiKey @""
+#define kTwitterCallbackURL @""
+#define kTwitterConsumerKey @""
+#define kTwitterConsumerSecret @""
